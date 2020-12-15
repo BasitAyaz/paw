122c12707da5a7880eab53133eff003140296bbd
BasitAhmed <basitahmed1997@gmail.com>
2020-12-15 11:41:55 -0600

    todo complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import firebase from "firebase";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mytext: "",
+      arr: [],
+      datafromfirebase: [],
+    };
+  }
+  add(e) {
+    e.preventDefault();
+    const { mytext, arr } = this.state;
+    console.log(arr);
+    arr.push(mytext);
+    this.setState({ arr: this.state.arr });
+    firebase.database().ref("/").child("notes").push(mytext).then((success)=>{
+      this.setState({mytext:''})
+    });
+  }
+  componentDidMount() {
+    const { datafromfirebase } = this.state;
+    firebase
+      .database()
+      .ref("/")
+      .child("notes")
+      .on("child_added", (e) => {
+        datafromfirebase.push(e.val());
+      });
+    this.setState({ datafromfirebase: this.state.datafromfirebase });
+  }
+  render() {
+    console.log(this.state.mytext);
+    return (
+      <div className="App">
+        <div className="main ">
+          <div className="container">
+            <ul>
+              {this.state.datafromfirebase.map((e, i) => {
+                return <li key={i}>{e}</li>;
+              })}
+            </ul>
+          </div>
+          <div>
+            <form onSubmit={(e) => this.add(e)}>
+              <input
+                value={this.state.mytext}
+                className="form-control"
+                placeholder="Enter Text"
+                onChange={(e) => this.setState({ mytext: e.target.value })}
+              />
+              <button className="rounded-pill btn btn-primary shadow">+</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase'
 
+
+
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCFPK01XcrMy8j1hO4Gg_S-jWe2SCeE4NE",
+  authDomain: "newchat-622a3.firebaseapp.com",
+  databaseURL: "https://newchat-622a3.firebaseio.com",
+  projectId: "newchat-622a3",
+  storageBucket: "newchat-622a3.appspot.com",
+  messagingSenderId: "65370138070",
+  appId: "1:65370138070:web:752b1564d6d4d5a60cf077"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -15,7 +31,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
